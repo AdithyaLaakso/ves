8100ff190f6619504e8222ae065c7bd29e290bb5
randsinger1 <rand.the.man.js@gmail.com>
2025-06-28 20:43:19 -0400

    got trivial model working

--- single_letter_model/dataset.py
+++ single_letter_model/dataset.py
@@ -3,16 +3,25 @@ IMG_PATH = 0
 LABEL = 1
 import json
 import numpy as np
+import torch
+from torch import Tensor
 from PIL import Image
+# TODO: Normalize images with resnet logic
+# TODO: Change sizes to 224x224
 class SingleLetterDataLoader:
     def __init__(self, data_path=DATA_PATH, batch_size=32, shuffle=True):
         self.data_path = data_path
         self.class_to_index = self.innit_class_name_to_index()
+        self.data_size = self.get_data_size()
         self.batch_size = batch_size
         self.shuffle = shuffle
-    def get_class_names(self):
+    def get_data_size(self):
         with open(self.data_path, "r") as f:
             data = json.load(f)['paths']
+        return len(data)
+    def get_class_names(self):
+        with open(self.data_path, "r") as f:
+            data = json.load(f)['paths'][:1000]
         labels = [item[LABEL] for item in data]
         return list(set(labels))
     def innit_class_name_to_index(self):
@@ -24,7 +33,7 @@ class SingleLetterDataLoader:
         return self.class_to_index.get(label, -1)
     def __iter__(self):
         with open(self.data_path, "r") as f:
-            data = json.load(f)['paths']
+            data = json.load(f)['paths'][:1000]
         if self.shuffle:
             np.random.shuffle(data)
         for i in range(0, len(data), self.batch_size):
@@ -38,4 +47,6 @@ class SingleLetterDataLoader:
                 if img.shape != img_shape:
                     raise ValueError(f"Image shape mismatch: expected {img_shape}, got {img.shape}")
             labels = [self.class_to_index[item[LABEL]] for item in batch_data]
-            yield np.array(imgs), np.array(labels)
+            # reshape images to (batch_size, channels, height, width)
+            imgs = [np.transpose(img, (2, 0, 1)) for img in imgs]
+            yield torch.tensor(imgs, dtype=torch.float32), torch.tensor(labels, dtype = torch.long)
