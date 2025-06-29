56749ef17dc33277fc9ec37745872ceb516a8eb1
randsinger1 <rand.the.man.js@gmail.com>
2025-06-28 20:43:19 -0400

    got trivial model working

--- single_letter_model/train.py
+++ single_letter_model/train.py
@@ -19,18 +19,23 @@ model = SingleLetterModel()
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+
+# TODO: ADD TRAINING LOGIC
 # Training loop
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
+    i = 0
     for inputs, targets in train_loader:
         optimizer.zero_grad()
+        i += 1
+        print(i)
         outputs = model(inputs)
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
         running_loss += loss.item() * inputs.size(0)
-    epoch_loss = running_loss / len(train_loader.dataset)
+    epoch_loss = running_loss / 1000#train_loader.data_size
     print(f"Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}")
 
 # Save the trained model
