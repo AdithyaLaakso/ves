ac0ae389684f6d2c0a8b39a5836b9605634a952c
randsinger1 <rand.the.man.js@gmail.com>
2025-07-21 10:43:30 -0400

    fixed bug

--- letter_visualization_model/model.py
+++ letter_visualization_model/model.py
@@ -7,30 +7,26 @@ class SingleLetterModel(nn.Module):
     def __init__(self):
         super(SingleLetterModel, self).__init__()
         # Encoder
-        self.encoder = nn.Sequential(
+        self.model = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2),  # 128x128
+
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2),  # 64x64
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),  # 32x32
-        )
-        # Decoder
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),  # 32x32 -> 64x64
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),   # 64x64 -> 128x128
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),    # 128x128 -> 256x256
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, 3, kernel_size=3, padding=1),             # Final output, channels=3
-            nn.Sigmoid()
-        )
 
+            # Output image of shape (C, 32, 32)
+            nn.Conv2d(128, 3, kernel_size=1),  # Output 3 channels (RGB)
+            nn.Sigmoid()  # If you want output in [0,1] range
+        )
     def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+        return self.model(x)
