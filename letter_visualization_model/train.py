f8829089914986d5378ce4f759344671d5f1bb28
randsinger1 <rand.the.man.js@gmail.com>
2025-07-21 09:58:14 -0400

    added more changes to model to discourage white pixels

--- letter_visualization_model/train.py
+++ letter_visualization_model/train.py
@@ -24,20 +24,17 @@ model.to(device)
 # criterion = torch.nn.MSELoss()  # For image reconstruction
 # create criterion that compares output and target images with bias against white pixels
 class BiasedMSELoss(torch.nn.Module):
-    def __init__(self, white_threshold=0.95, bias_factor=6.0):
+    def __init__(self, bias_factor=6.0):
         super().__init__()
-        self.white_threshold = white_threshold
         self.bias_factor = bias_factor
 
     def forward(self, output, target):
-        # Assume output and target are in [0,1], shape (batch, channels, H, W)
+        # Penalize the model for predicting white pixels (output close to 1)
         mse = (output - target) ** 2
-        # Mask: where output is "white" but target is "dark"
-        white_guess = output > self.white_threshold
-        dark_target = target < (1.0 - self.white_threshold)
-        bias_mask = white_guess & dark_target
-        mse[bias_mask] *= self.bias_factor
-        return mse.mean()
+        penalty_by_pixel = output * self.bias_factor + 1
+        mse *= penalty_by_pixel
+        reg = torch.mean(output)
+        return mse.mean() + (reg - .7) * 1.7
 
 criterion = BiasedMSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
