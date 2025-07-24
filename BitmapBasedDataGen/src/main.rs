75414e30f4f1c945648df1f91d09e9c29097f48f
AdithyaLaakso <adithya.laakso@gmail.com>
2025-07-24 10:59:52 -0400

    updated bitmap generation for levels

--- BitmapBasedDataGen/src/main.rs
+++ BitmapBasedDataGen/src/main.rs
@@ -1,4 +1,6 @@
-use std::io;
+use std::io::{self, Write};
+
+use rand::Rng;
 
 use rustbitmap::BitMap;
 use rustbitmap::Rgba;
@@ -6,9 +8,13 @@ use rustbitmap::Rgba;
 use libnoise::prelude::*;
 
 const SIZE: u32 = 128;
-const PATH: &str = "/home/Adithya/Documents/ves/hand_writing_dataset/";
-const RAW_DATA_PATH: &str = "/home/Adithya/Documents/greek_training/raw_data/";
-const OUT_DIR: &str = "/home/Adithya/Documents/greek_training/data/";
+const OG_SIZE: u32 = 32;
+const F_SIZE: f64 = (SIZE as f64) / 100.;
+const PERLIN_SCALE: [f64; 2] = [F_SIZE; 2];
+const DIMS: [usize; 2] = [SIZE as usize, SIZE as usize];
+const NOISE_FACTOR: f64 = 3./4.;
+const LEVELS: usize = 30;
+const RAW_DATA_PATH: &str = "/home/Adithya/Documents/ves/raw_data/";
 
 fn upscale_dir(dir_path: &str, size: u32) -> Result<(), Box< dyn std::error::Error>> {
     for dir_type in std::fs::read_dir(dir_path)? {
@@ -50,10 +56,25 @@ fn load_map(map_path: &std::path::Path) -> BitMap {
     return map;
 }
 
+fn tex_to_map(tex: &NoiseBuffer<2>) -> Box<BitMap> {
+    let mut map = BitMap::new(SIZE, SIZE);
+    for x in 0..SIZE {
+        for y in 0..SIZE {
+            let weight = (tex[[x as usize, y as usize]] * 255.0) as u8;
+            let rgb_weight = Rgba::rgb(weight, weight, weight);
+            let _ = map.set_pixel(x, y, rgb_weight);
+        }
+    }
+
+    Box::new(map)
+}
+
+
 fn mask_bitmap(mask: &BitMap,
     mask_in_texture: &NoiseBuffer<2>,
     mask_out_texture: &NoiseBuffer<2>,
-    size: u32
+    size: u32,
+    noise_reduction: f64
 ) -> Box<BitMap> {
     let outsize = size.try_into().unwrap();
     let mut new_map = BitMap::new(outsize, outsize);
@@ -68,9 +89,9 @@ fn mask_bitmap(mask: &BitMap,
 
             let texture_xy: f64;
             if out {
-                texture_xy = (mask_out_texture[[xu,yu]] + 1.0) / 2.0;
+                texture_xy = (((mask_out_texture[[xu,yu]] + 1.0) / 2.0) + (1. - noise_reduction)).clamp(0.0, 1.0);
             } else {
-                texture_xy = (mask_in_texture[[xu,yu]] + 1.0) / 2.0;
+                texture_xy = (((mask_in_texture[[xu,yu]] + 1.0) / 2.0) - (noise_reduction)).clamp(0.0, 1.0);
             }
 
             let pixel_value: u8 = (255.0 * texture_xy) as u8;
@@ -82,35 +103,125 @@ fn mask_bitmap(mask: &BitMap,
     Box::new(new_map)
 }
 
-fn main() -> io::Result<()> {
-    for file in std::fs::read_dir(RAW_DATA_PATH)? {
-        let fp = file?.path();
-        let mask: BitMap = load_map(&fp);
-        let noise1: u32 = rand::random_range(0..7);
-        let noise2: u32 = rand::random_range(0..7);
+fn apply_texture(map: &mut BitMap, tex: &Box<NoiseBuffer<2>>, level: f64) -> () {
+    for x in 0..SIZE {
+        for y in 0..SIZE {
+            let xu: usize = x as usize;
+            let yu: usize = y as usize;
+            let map_pixel = map.get_pixel(x, y).unwrap();
 
-        let noise1_seed = rand::random_range(0..1000);
-        let noise2_seed = rand::random_range(0..1000);
+            let map_pixel_depth_float: f64 =
+                (map_pixel.get_red() as f64 +
+                 map_pixel.get_blue() as f64 +
+                 map_pixel.get_green() as f64) / (3.0 * 255.0);
 
-        let dims = [SIZE as usize, SIZE as usize];
+            let tex_pixel = tex[[xu, yu]];
+            let noise_factor = NOISE_FACTOR * level;
+            let new_pixel = ((map_pixel_depth_float) + (tex_pixel * noise_factor)) / (noise_factor + 1.0);
 
-       let source1 = Source::simplex(noise1_seed);
-       let source2 = Source::simplex(noise2_seed);
-       let noises = (
-           Box::new(NoiseBuffer::<2>::new(dims, &source1)),
-           Box::new(NoiseBuffer::<2>::new(dims, &source2))
-        );
+            let color_value: u8 = (new_pixel * 255.0).clamp(0.0, 255.0) as u8;
+            let new_rgba = Rgba::rgb(color_value, color_value, color_value);
 
-        let (bg_tex, mask_tex) = noises;
-
-        let new_bmp = mask_bitmap(&mask, &mask_tex, &bg_tex, SIZE);
-
-        let mut file_name = fp.file_name().unwrap().to_str().unwrap().to_owned();
-        file_name = OUT_DIR.to_owned() + &file_name;
-
-        println!("Saving to... {file_name}");
-        let _ = new_bmp.save_as(file_name.as_str());
+            let _ = map.set_pixel(x, y, new_rgba);
+        }
     }
+}
 
+enum NoiseSource {
+    Simplex(Simplex<2>),
+    Perlin(Mul<2, Add<2, Scale<2, Perlin<2>>>>),
+    ImprovedPerlin(Mul<2, Add<2, Scale<2, ImprovedPerlin<2>>>>),
+    Worley(Worley<2>),
+    Value(Value<2>),
+}
+
+impl NoiseSource {
+    fn sample(&self, point: [f64; 2]) -> f64 {
+        match self {
+            NoiseSource::Simplex(source) => source.sample(point),
+            NoiseSource::Perlin(source) => source.sample(point),
+            NoiseSource::ImprovedPerlin(source) => source.sample(point),
+            NoiseSource::Worley(source) => source.sample(point),
+            NoiseSource::Value(source) => source.sample(point),
+        }
+    }
+    fn get_buffer_box(&self) -> Box<NoiseBuffer<2>> {
+        match self {
+            NoiseSource::Simplex(source) => Box::new(NoiseBuffer::<2>::new(DIMS, source)),
+            NoiseSource::Perlin(source) => Box::new(NoiseBuffer::<2>::new(DIMS, source)),
+            NoiseSource::ImprovedPerlin(source) => Box::new(NoiseBuffer::<2>::new(DIMS, source)),
+            NoiseSource::Worley(source) => Box::new(NoiseBuffer::<2>::new(DIMS, source)),
+            NoiseSource::Value(source) => Box::new(NoiseBuffer::<2>::new(DIMS, source)),
+        }
+    }
+}
+
+fn get_random_source(seed: u64) -> NoiseSource {
+    let random = rand::rng().random_range(0..4);
+    let source: NoiseSource = match random {
+        0 => NoiseSource::Simplex(Source::simplex(seed)),
+        1 => NoiseSource::Perlin(Source::perlin(seed).scale(PERLIN_SCALE).add(1.).mul(0.5)),
+        2 => NoiseSource::ImprovedPerlin(Source::improved_perlin(seed).scale(PERLIN_SCALE).add(1.).mul(0.5)),
+        3 => NoiseSource::Worley(Source::worley(seed)),
+        _ => {
+            NoiseSource::Value(Source::value(seed))
+        }
+    };
+
+    source
+}
+
+fn crop_and_resize(map: &mut BitMap, fx: u32 , tx: u32, fy: u32, ty: u32, resize: u32) -> () {
+    let _ = map.crop(fx, fy, tx, ty);
+    map.slow_resize_to(resize, resize);
+}
+
+fn main() -> io::Result<()> {
+    let dir: Vec<_> = std::fs::read_dir(RAW_DATA_PATH)?
+    .collect::<Result<Vec<_>, _>>()?;
+    let file_count = dir.len();
+    //     let noise_count = (file_count / NOISE_RATIO_DENOMINATOR) as usize;
+    //     let count = file_count + noise_count;
+    let count = file_count;
+
+    println!("(0/{count} working!");
+    //     for level in 0..LEVELS {
+    for level in 1..=LEVELS {
+        for (file_numer, file) in dir.iter().enumerate() {
+
+            //Step 1: load two copies of the base data
+            let fp = file.path();
+            let mask: BitMap = load_map(&fp);
+            let og_copy: BitMap = load_map(&fp);
+            let file_name = fp.file_name().unwrap().to_str().unwrap().to_owned();
+            let og: String = format!("/Windows/training_data/progressive_test/level{level}/og/{file_name}");
+            let noisy: String = format!("/Windows/training_data/progressive_test/level{level}/noisy/{file_name}");
+
+            //Step 3: Save the og
+            let _ = og_copy.save_as(&og);
+
+            // Step 4: generate and apply noise
+            //         let noise1_seed = rand::random_range(0..std::u64::MAX);
+            //         let noise2_seed = rand::random_range(0..std::u64::MAX);
+            //         let source1 = get_random_source(noise1_seed);
+            //         let source2 = get_random_source(noise2_seed);
+            //         let (bg_tex, mask_tex) = (
+            //             source1.get_buffer_box(),
+            //             source2.get_buffer_box()
+            //         );
+            let noise_level = (level as f64) / (LEVELS as f64);
+            //         let mut new_bmp = mask_bitmap(&mask, &mask_tex, &bg_tex, SIZE, noise_level);
+            let noise3_seed = rand::random_range(0..std::u64::MAX);
+            let mut new_bmp = mask;
+            let source3 = get_random_source(noise3_seed);
+            apply_texture(&mut new_bmp, &source3.get_buffer_box(), noise_level as f64);
+
+            //Step 5: Save the noisy
+//             print!("(\r ({file_numer}/{count}) Saving to... {file_name}");
+            io::stdout().flush().unwrap();
+            let _ = new_bmp.save_as(&noisy);
+        }
+    }
+    //     }
     Ok(())
 }
