450775054e12c00b9f864f581e95b48afa98034f
AdithyaLaakso <adithya.laakso@gmail.com>
2025-09-25 12:13:02 -0400

    trying it again on just level 1

--- letter_visualization_model/settings.py
+++ letter_visualization_model/settings.py
@@ -79,7 +79,7 @@ data_path = "/home/Adithya/Documents/synthetic_ct_images/paths.json"
 add_to_path = "/home/Adithya/Documents/"
 
 # levels = [[i for i in range(0,5)]]
-levels = [[i for i in range(0, 5)]]
+levels = [1]
 
 # display_levels = levels[0]
 display_levels = levels
