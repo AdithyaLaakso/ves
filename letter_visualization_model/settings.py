4c644c3fc8a834dc27e2bb080a26ae002b23ed4f
AdithyaLaakso <adithya.laakso@gmail.com>
2025-09-25 16:49:43 -0400

    trained on level 1, lets try it on levels 0-5

--- letter_visualization_model/settings.py
+++ letter_visualization_model/settings.py
@@ -78,8 +78,8 @@ persistent_workers=False
 data_path = "/home/Adithya/Documents/synthetic_ct_images/paths.json"
 add_to_path = "/home/Adithya/Documents/"
 
-# levels = [[i for i in range(0,5)]]
-levels = [1]
+levels = [[i for i in range(0,5)]]
+# levels = [1]
 
 # display_levels = levels[0]
 display_levels = levels
@@ -103,7 +103,7 @@ save_every_epoch = True
 save_to = "/home/Adithya/Documents/ves/letter_visualization_model/new.pth"
 # load_from = "/home/Adithya/Documents/ves/letter_visualization_model/saved_models/thismodelisverygood.pth"
 # load_from = None
-load_from = "/home/Adithya/Documents/ves/letter_visualization_model/pretrainedonclean.pth"
+load_from = "/home/Adithya/Documents/ves/letter_visualization_model/pretrainlevel1.pth"
 
 display_from = save_to
 # display_from = "/home/Adithya/Documents/ves/letter_visualization_model/checkpoints/18-2.pth"
